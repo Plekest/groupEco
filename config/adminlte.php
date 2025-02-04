dc34875ea3466148617e067194a0a6a38d848753
Pedro Teles <phtf.pedro12@gmail.com>
2025-02-03 23:59:32 -0300

    create crud for EG

--- config/adminlte.php
+++ config/adminlte.php
@@ -321,8 +321,8 @@ return [
             'can' => 'manage-blog',
         ],
         [
-            'text' => 'pages',
-            'url' => 'admin/pages',
+            'text' => 'Grupos Econômicos',
+            'url' => 'economic_groups/',
             'icon' => 'far fa-fw fa-file',
             'label' => 4,
             'label_color' => 'success',
