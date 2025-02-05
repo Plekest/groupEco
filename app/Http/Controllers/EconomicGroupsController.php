d2ca1c551b26c921f56d6e459d6a5fa6fea3f009
Pedro Teles <phtf.pedro12@gmail.com>
2025-02-05 00:11:50 -0300

    limpando controller

--- app/Http/Controllers/EconomicGroupsController.php
+++ app/Http/Controllers/EconomicGroupsController.php
@@ -3,7 +3,6 @@
 namespace App\Http\Controllers;
 
 use App\Models\EconomicGroup;
-use Illuminate\Http\Request;
 
 class EconomicGroupsController extends Controller
 {
@@ -14,62 +13,5 @@ class EconomicGroupsController extends Controller
     {
         $economicGroups = collect(EconomicGroup::all());
         return view('economic_groups.index', compact('economicGroups'));
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(Request $request)
-    {
-        // dd($request->all());
-        $request->validate([
-            'name' => ['required', 'string', 'max:255'],
-        ]);
-
-        $economicGroup = EconomicGroup::create([
-            'name' => $request->name
-        ]);
-
-        return response()->json($economicGroup);
-    }
-
-    /**
-     * Display the specified resource.
-     */
-    public function show(string $id)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(string $id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, string $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(string $id)
-    {
-        //
     }
 }
