01f4a9d74c409239ff4e6b9ca34b7a43ddae3200
Pedro Teles <phtf.pedro12@gmail.com>
2025-02-05 00:16:55 -0300

    tela de atualizar senha

--- resources/views/profile/partials/update-password-form.blade.php
+++ resources/views/profile/partials/update-password-form.blade.php
@@ -1,11 +1,10 @@
 <section>
     <header>
         <h2 class="text-lg font-medium text-gray-900 dark:text-gray-100">
-            {{ __('Update Password') }}
+            Atualizar Senha
         </h2>
-
         <p class="mt-1 text-sm text-gray-600 dark:text-gray-400">
-            {{ __('Ensure your account is using a long, random password to stay secure.') }}
+            Certifique-se de usar uma senha longa e aleatória para manter sua conta segura.
         </p>
     </header>
 
@@ -13,36 +12,47 @@
         @csrf
         @method('put')
 
-        <div>
-            <x-input-label for="update_password_current_password" :value="__('Current Password')" />
-            <x-text-input id="update_password_current_password" name="current_password" type="password" class="mt-1 block w-full" autocomplete="current-password" />
-            <x-input-error :messages="$errors->updatePassword->get('current_password')" class="mt-2" />
+        <div class="form-group align-items-center">
+            <label for="update_password_current_password" class="col-sm-2 col-form-label">Senha Atual</label>
+            <div class="col-sm-10">
+                <input id="update_password_current_password" name="current_password" type="password"
+                    class="form-control" autocomplete="current-password" />
+                @if ($errors->updatePassword->has('current_password'))
+                    <span class="text-danger">{{ $errors->updatePassword->first('current_password') }}</span>
+                @endif
+            </div>
         </div>
 
-        <div>
-            <x-input-label for="update_password_password" :value="__('New Password')" />
-            <x-text-input id="update_password_password" name="password" type="password" class="mt-1 block w-full" autocomplete="new-password" />
-            <x-input-error :messages="$errors->updatePassword->get('password')" class="mt-2" />
+        <div class="form-group align-items-center">
+            <label for="update_password_password" class="col-sm-2 col-form-label">Nova Senha</label>
+            <div class="col-sm-10">
+                <input id="update_password_password" name="password" type="password" class="form-control"
+                    autocomplete="new-password" />
+                @if ($errors->updatePassword->has('password'))
+                    <span class="text-danger">{{ $errors->updatePassword->first('password') }}</span>
+                @endif
+            </div>
         </div>
 
-        <div>
-            <x-input-label for="update_password_password_confirmation" :value="__('Confirm Password')" />
-            <x-text-input id="update_password_password_confirmation" name="password_confirmation" type="password" class="mt-1 block w-full" autocomplete="new-password" />
-            <x-input-error :messages="$errors->updatePassword->get('password_confirmation')" class="mt-2" />
+        <div class="form-group align-items-center">
+            <label for="update_password_password_confirmation" class="col-sm-2 col-form-label">Confirmar Senha</label>
+            <div class="col-sm-10">
+                <input id="update_password_password_confirmation" name="password_confirmation" type="password"
+                    class="form-control" autocomplete="new-password" />
+                @if ($errors->updatePassword->has('password_confirmation'))
+                    <span class="text-danger">{{ $errors->updatePassword->first('password_confirmation') }}</span>
+                @endif
+            </div>
         </div>
 
-        <div class="flex items-center gap-4">
-            <x-primary-button>{{ __('Save') }}</x-primary-button>
-
-            @if (session('status') === 'password-updated')
-                <p
-                    x-data="{ show: true }"
-                    x-show="show"
-                    x-transition
-                    x-init="setTimeout(() => show = false, 2000)"
-                    class="text-sm text-gray-600 dark:text-gray-400"
-                >{{ __('Saved.') }}</p>
-            @endif
+        <div class="form-group">
+            <div class="col-sm-10 mt-4">
+                <button type="submit" class="btn btn-success">Salvar</button>
+                @if (session('status') === 'password-updated')
+                    <p x-data="{ show: true }" x-show="show" x-transition x-init="setTimeout(() => show = false, 2000)"
+                        class="text-sm text-gray-600 dark:text-gray-400 ms-3">Salvo com Sucesso!</p>
+                @endif
+            </div>
         </div>
     </form>
 </section>
